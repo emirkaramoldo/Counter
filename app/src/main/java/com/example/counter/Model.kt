656169d6de1d0a3d5d50de2bea606bb28bbbac18
Emir Karamoldoev <emirkaramoldoev1@gmail.com>
2023-09-21 14:46:45 +0600

    Выполнено 1 задание

--- app/src/main/java/com/example/counter/Model.kt
+++ app/src/main/java/com/example/counter/Model.kt
@@ -1,12 +1,7 @@
 package com.example.counter
 
-import android.graphics.Color
-import android.widget.Toast
-
 class Model() {
     var count = 0
-    lateinit var message: Toast
-    lateinit var color: Color
 
     fun increment() {
         ++count
